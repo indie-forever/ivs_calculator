64b2c6b953ee809fb292ef799b99fd0bfda0cdb9
danielf40k <daniel.df.forman@gmail.com>
2026-03-30 21:28:28 +0200

    uprava

--- src/main.py
+++ src/main.py
@@ -4,10 +4,8 @@ from gui import CalculatorUI
 def main(page: ft.Page):
     page.title = "Moje Kalkulačka"
     
-    # 1. Vytvoříme instanci tvého UI
     ui = CalculatorUI()
     
-    # 2. SEM VLOŽÍME TU FUNKCI (jako vnitřní funkci)
     def handle_click(e):
         napsano_na_tlacitku = e.control.data 
         
@@ -18,11 +16,8 @@ def main(page: ft.Page):
         
         page.update()
 
-    # 3. PROPOJENÍ: Musíme říct tvým tlačítkům, aby tuto funkci volala.
-    # V gui.py jsme si připravili on_btn_click, tak mu ji teď předáme.
     ui.on_btn_click = handle_click 
 
-    # 4. Přidáme celé UI na stránku
     page.add(ui.get_view())
 
 ft.app(target=main)

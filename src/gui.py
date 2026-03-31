2ff0c3e06bf00ece258f4c9c8dced6b2c14315f8
danielf40k <daniel.df.forman@gmail.com>
2026-03-31 20:10:15 +0200

    pridani tlacitek a resize

--- src/gui.py
+++ src/gui.py
@@ -4,8 +4,8 @@ class CalculatorUI:
     def __init__(self):
         self.bg_color = "black"
         self.display_color = "white"
-        
         self.display = ft.Text(value="0", size=40, color="white")
+        self.on_btn_click = None
 
     def build_button(self, text, color="orange", text_color="white"):
         return ft.Container(
@@ -14,43 +14,60 @@ class CalculatorUI:
             border_radius=10,
             alignment=ft.Alignment(0, 0), 
             expand=1,
-            height=70,
-            on_click=self.on_btn_click,
+            height=90,
+            on_click=lambda e: self.on_btn_click(e) if self.on_btn_click else None,
             data=text
         )
 
-    def on_btn_click(self, e):
-        print(f"Stisknuto: {e.control.data}")
-
     def get_view(self):
         return ft.Column(
+            expand=True,
+            spacing=10,
             controls=[
-                ft.Container(
-                content=self.display, 
-                padding=20, 
-                alignment=ft.Alignment(1, 0),
-                bgcolor="grey900",  
-                border_radius=10         
-            ),
-                ft.Row(controls=[
-                    self.build_button("7"), self.build_button("8"), 
-                    self.build_button("9"), self.build_button("/")
-                ]),
-                ft.Row(controls=[
-                    self.build_button("4"), self.build_button("5"), 
-                    self.build_button("6"), self.build_button("*")
-                ]),
-                ft.Row(controls=[
-                    self.build_button("1"), self.build_button("2"), 
-                    self.build_button("3"), self.build_button("-")
-                ]),
-                ft.Row(controls=[
-                    # U "C" můžeme barvu přebít na červenou, pokud chceš
-                    self.build_button("C", color="red"), 
-                    self.build_button("0"), 
-                    self.build_button("="), 
-                    self.build_button("+")
-                ]),
-            ],
-            width=350
+                ft.Row(
+                    controls=[
+                        ft.Container(
+                            content=self.display, 
+                            padding=20, 
+                            bgcolor="grey900",
+                            alignment=ft.Alignment(1, 0),
+                            border_radius=10,
+                            height=150,
+                            expand=True
+                        )
+                    ]
+                ),
+                ft.Row(
+                    spacing=10,
+                    controls=[
+                        self.build_button("7"), self.build_button("8"), 
+                        self.build_button("9"), self.build_button("/"),
+                        self.build_button("^")
+                    ]
+                ),
+                ft.Row(
+                    spacing=10,
+                    controls=[
+                        self.build_button("4"), self.build_button("5"), 
+                        self.build_button("6"), self.build_button("*"),
+                        self.build_button("√")
+                    ]
+                ),
+                ft.Row(
+                    spacing=10,
+                    controls=[
+                        self.build_button("1"), self.build_button("2"), 
+                        self.build_button("3"), self.build_button("-"),
+                        self.build_button("(")
+                    ]
+                ),
+                ft.Row(
+                    spacing=10,
+                    controls=[
+                        self.build_button("C", color="red"), 
+                        self.build_button("0"), self.build_button("="), 
+                        self.build_button("+"), self.build_button(")")
+                    ]
+                ),
+            ]
         )
--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 import flet as ft
 from gui import CalculatorUI  
 
+
 def main(page: ft.Page):
     page.title = "Moje Kalkulačka"
+
+    page.theme_mode = ft.ThemeMode.DARK 
+
+    page.padding = 5
+    
+    page.window_width = 390   # Ubrali jsme cca 70 pixelů
+    page.window_height = 450  # Ubrali jsme cca 30 pixelů
+    
+    page.window_resizable = False
     
     ui = CalculatorUI()
     
